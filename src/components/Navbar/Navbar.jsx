835d56eb5aab282763b9cb2546215d01dd3473c8
TareqJarwan <tjarwan22@gmail.com>
2022-05-05 05:32:54 +0300

    hotfix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 
-import images from '../../constants/images';
+import { gericht } from '../../constants/images';
 
 import './Navbar.css';
 
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
-        <img src={images.gericht} alt='app logo' />
+        <img src={gericht} alt='app logo' />
       </div>
 
       <ul className='app__navbar-links'>
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,27 @@
-import images from './images';
+import {
+    bg,
+    chef,
+    G,
+    gallery01,
+    gallery02,
+    gallery03,
+    gallery04,
+    knife,
+    logo,
+    menu,
+    overlaybg,
+    spoon,
+    welcome,
+    findus,
+    laurels,
+    award01,
+    award02,
+    award03,
+    award05,
+    sign,
+    quote,
+    gericht,
+} from './images';
 import meal from '../assets/meal.mp4';
 import { awards, cocktails, wines } from './data';
 
@@ -6,6 +29,31 @@ const data = {
     awards,
     cocktails,
     wines,
+}
+
+const images = {
+    bg,
+    chef,
+    G,
+    gallery01,
+    gallery02,
+    gallery03,
+    gallery04,
+    knife,
+    logo,
+    menu,
+    overlaybg,
+    spoon,
+    welcome,
+    findus,
+    laurels,
+    award01,
+    award02,
+    award03,
+    award05,
+    sign,
+    quote,
+    gericht,
 }
 
 export { images, meal, data };
